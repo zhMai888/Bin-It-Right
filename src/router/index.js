de8a53218e3c2c00a3d42febc855d4f00baf033a
Zhihao Mai <zhMai1962132486@163.com>
2025-06-04 17:03:22 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
-import Vue from 'vue'
+import Vue, { KeepAlive } from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import index from '../views/index.vue'
+import game from '../views/game.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'index',
+    component: index,
+    meta: {
+      requireAuth: false
+    }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/game',
+    name: 'game',
+    component: game,
+    meta: {
+      KeepAlive: true,
+      requireAuth: true
     }
   }
 ]
